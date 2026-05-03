12dd5d36b01cf5210a3b22afdda1d97341237b1a
Tomas Pandullo <64372041+Tomy97@users.noreply.github.com>
2026-05-03 15:03:25 -0300

    Update angular.json to include prerender configuration and enhance app.routes.ts with a catch-all redirect to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,22 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/authGuard.guard';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import('./features/auth/pages/login/login').then(m => m.Login)
-    },
-    {
-        canActivate: [authGuard],
-        path: 'feed',
-        loadComponent: () => import('./features/feed/pages/feed/feed').then(m => m.Feed)
-    },
-    {
-        path: '',
-        redirectTo: 'feed',
-        pathMatch: 'full'
-    }
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/pages/login/login').then((m) => m.Login),
+  },
+  {
+    canActivate: [authGuard],
+    path: 'feed',
+    loadComponent: () => import('./features/feed/pages/feed/feed').then((m) => m.Feed),
+  },
+  {
+    path: '',
+    redirectTo: 'feed',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
